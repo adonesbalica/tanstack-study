e80ce5e3c8b4866b604b468f877f2d1263fc162d
Adones Balica <adonesbalica@gmail.com>
2026-07-02 12:25:59 -0300

    feat: integrate @tanstack/react-query and sonner for enhanced data fetching and notifications
    - Added @tanstack/react-query and sonner dependencies to package.json and package-lock.json.
    - Implemented QueryClient and QueryClientProvider in the Providers component for managing server state.
    - Created a new Toaster component using sonner for displaying notifications with custom icons and styles.

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -2,20 +2,38 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (e) => {
+        if (e.message === "NEXT_REDIRECT") return;
+        toast.error(e.message);
+      },
+      onSuccess: () => {
+        toast.error("Operation was successful.");
+      },
+    },
+  },
+});
+
 export function Providers({ children }: ProvidersProps) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <NextThemesProvider>{children}</NextThemesProvider>
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <NextThemesProvider>{children}</NextThemesProvider>
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 }
