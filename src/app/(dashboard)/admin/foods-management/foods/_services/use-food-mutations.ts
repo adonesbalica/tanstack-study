c78f8dc9cd2ca1c937d037b002970d98bdc486cd
Adones Balica <adonesbalica@gmail.com>
2026-07-02 17:29:36 -0300

    feat: implement food management with CRUD functionality
    - Introduced components and services for managing food items, including state management with `useFoodsStore`.
    - Implemented server-side functions for creating, updating, and deleting food items using Prisma.
    - Added query hooks for fetching food data and integrated with `@tanstack/react-query` for enhanced data handling.
    - Established validation schemas for food and filter inputs using Zod.
    - Enhanced user experience with notifications for CRUD operations and improved data fetching mechanisms.

---
+++ src/app/(dashboard)/admin/foods-management/foods/_services/use-food-mutations.ts
@@ -0,0 +1,48 @@
+import { FoodSchema } from "@/app/(dashboard)/admin/foods-management/foods/_types/foodSchema";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { createFood, deleteFood, updateFood } from "./foodMutation";
+
+const useCreateFood = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: FoodSchema) => {
+      await createFood(data);
+    },
+    onSuccess: () => {
+      toast.success("Food created successfully.");
+      queryClient.invalidateQueries({ queryKey: ["foods"] });
+    },
+  });
+};
+
+const useUpdateFood = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: FoodSchema) => {
+      await updateFood(data);
+    },
+    onSuccess: () => {
+      toast.success("Food updated successfully.");
+      queryClient.invalidateQueries({ queryKey: ["foods"] });
+    },
+  });
+};
+
+const useDeleteFood = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (id: string) => {
+      await deleteFood(id);
+    },
+    onSuccess: () => {
+      toast.success("Food deleted successfully.");
+      queryClient.invalidateQueries({ queryKey: ["foods"] });
+    },
+  });
+};
+
+export { useCreateFood, useDeleteFood, useUpdateFood };
