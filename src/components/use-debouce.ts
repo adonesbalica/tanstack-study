dfcf654ff7cea0858cf78936237d9ba0a9eb6b3a
Adones Balica <adonesbalica@gmail.com>
2026-07-03 08:07:04 -0300

    feat: add controlled slider component and debounce hook
    - Introduced `ControlledSlider` component utilizing `@radix-ui/react-slider` for enhanced user interaction.
    - Added `useDebouce` hook for managing debounced state updates.
    - Updated package dependencies in `package.json` and `package-lock.json` to include `@radix-ui/react-slider`.

---
+++ src/components/use-debouce.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const useDebouce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export { useDebouce };
